c2276e5cea9b69557f851f5289cf001381e23b48
Amam Dewan <cybercrafttech@gmail.com>
2021-02-16 01:58:55 +0600

    innitial commit

---
+++ eShop.php
@@ -0,0 +1,198 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title> eShop </title>
+	<link rel="stylesheet" href="css/basic.css">
+	<link rel="stylesheet" href="css/search.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+	<img id="logo" src="img/logo.jpg">
+	
+	<form class="searchbar" action="/action_page.php" style="margin:auto;max-width:300px">
+		<input type="text" placeholder="Search.." name="search2">
+		<button type="submit"><i class="a-search"><img src="img/search.png" style="width:16px;" ></i></button>
+	</form>
+	
+	<ul class="nav1">
+		<li class="navbar"><a href="home.html">Home</a></li>
+		<li class="navbar"><a href="#news">All-Items</a></li>
+		<li class="navbar"><a href="#contact">Track-Order</a></li>
+		<li class="navbar" style="float:right"><a class="active" href="#about">eBag</a></li>
+		<li class="navbar" style="float:right"><a class="active" href="#about">MyOrders</a></li>
+		<li class="navbar" style="float:right"><a class="active" href="#about">Wishlist</a></li>
+	</ul>
+
+<section class="sec">	
+	<h2 class="header"> Collection </h2>
+	
+	<div class="card">
+		<p class="inf">Kajaru Leather Watch Men</p>
+		<img class="img" src="img/collection/1.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00$ <img class="cart" src="img/addtocart.png" ></p>
+		
+	</div>
+	<div class="card">
+		<p class="inf">Black Leather Bag Women</p>
+		<img class="img" src="img/collection/bag3.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00$<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Nike Shoes Women</p>
+		<img class="img" src="img/collection/shoe4.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00$<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Printed Womens Winter Jacket</p>
+		<img class="img" src="img/collection/7.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Sweet Womens High Heel</p>
+		<img class="img" src="img/collection/shoe2.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Korean Womens Blouse</p>
+		<img class="img" src="img/collection/1.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Gold Wrist Watch Womens</p>
+		<img class="img" src="img/collection/2.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Formal Two Pieces Womens</p>
+		<img class="img" src="img/collection/2.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Quartz Thin Watch Womens</p>
+		<img class="img" src="img/collection/3.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Kabir Singh SunGlass Men</p>
+		<img class="img" src="img/collection/4.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">SweatShirt Womens</p>
+		<img class="img" src="img/collection/6.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Gucci Ankle Boot Womens</p>
+		<img class="img" src="img/collection/shoe3.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+</section >
+	
+<section class="sec">	
+	<h2 class="header"> Kids </h2>
+	<div class="card">
+		<p class="inf">Gold Bangles Set Women</p>
+		<img class="img" src="img/kids/accessories/1.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Smart Watch Women</p>
+		<img class="img" src="img/kids/bags/2.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Fancy Leather BagPack</p>
+		<img class="img" src="img/kids/kids wear/3.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Pumps Heel Women</p>
+		<img class="img" src="img/kids/shoes/3.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Ankle Boots Women</p>
+		<img class="img" src="img/kids/accessories/5.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Stretchy Gym Tights Women</p>
+		<img class="img" src="img/kids/bags/6.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+</section>
+
+
+<section class="sec">	
+	<h2 class="header"> Mens </h2>
+	<div class="card">
+		<p class="inf">BWC Mens Watch</p>
+		<img class="img" src="img/men/accessories/1.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Italian Burnished Belt Men</p>
+		<img class="img" src="img/men/accessories/6.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">UV Protected Googles</p>
+		<img class="img" src="img/men/accessories/2.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Vintage Luggage Mens</p>
+		<img class="img" src="img/men/bags/4.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Vintage Leather Bagpack Mens</p>
+		<img class="img" src="img/men/bags/1.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Leather Side Bagpack Men</p>
+		<img class="img" src="img/men/bags/3.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+</section>
+	
+<section class="sec">	
+	<h2 class="header"> Womens </h2>
+	<div class="card">
+		<p class="inf">Gold Bangles Set Women</p>
+		<img class="img" src="img/women/accessories/1.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Smart Watch Women</p>
+		<img class="img" src="img/women/accessories/7.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Fancy Leather BagPack</p>
+		<img class="img" src="img/women/bags/bag5.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Pumps Heel Women</p>
+		<img class="img" src="img/women/shoes/shoe5.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Ankle Boots Women</p>
+		<img class="img" src="img/women/shoes/shoe7.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Stretchy Gym Tights Women</p>
+		<img class="img" src="img/women/wear/3.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+</section>
+
+
+</body>
+</html>
---
+++ womens.php
@@ -0,0 +1,164 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title> eShop - Womens </title>
+	<link rel="stylesheet" href="css/basic.css">
+	<link rel="stylesheet" href="css/search.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+	<img id="logo" src="img/logo.jpg">
+	
+	<form class="searchbar" action="/action_page.php" style="margin:auto;max-width:300px">
+		<input type="text" placeholder="Search.." name="search2">
+		<button type="submit"><i class="a-search"><img src="img/search.png" style="width:16px;" ></i></button>
+	</form>
+	
+	<ul class="nav1">
+		<li class="navbar"><a href="#home">Home</a></li>
+		<li class="navbar"><a href="#news">All-Shops</a></li>
+		<li class="navbar"><a href="#contact">Track-Order</a></li>
+		<li class="navbar" style="float:right"><a class="active" href="#about">eBag</a></li>
+		<li class="navbar" style="float:right"><a class="active" href="#about">MyOrders</a></li>
+		<li class="navbar" style="float:right"><a class="active" href="#about">Wishlist</a></li>
+	</ul>
+
+
+<section class="sec">	
+	<h2 class="header"> Womens Wear</h2>
+	<div class="card">
+		<p class="inf">Korean Womens Blouse</p>
+		<img class="img" src="img/women/wear/1.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Formal Two Pieces Womens</p>
+		<img class="img" src="img/women/wear/2.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Stretchy Gym Tights Womens</p>
+		<img class="img" src="img/women/wear/3.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Peplum Suits Womens</p>
+		<img class="img" src="img/women/wear/4.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Korean Velvet Dress Womens</p>
+		<img class="img" src="img/women/wear/5.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">SweatShirt Womens</p>
+		<img class="img" src="img/women/wear/6.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+</section>
+
+<section class="sec">	
+	<h2 class="header"> Womens Shoes</h2>
+	<div class="card">
+		<p class="inf">Glittery Peep Heels Womens</p>
+		<img class="img" src="img/women/shoes/shoe1.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Sweet Womens High Heel</p>
+		<img class="img" src="img/women/shoes/shoe2.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Gucci Ankle Boot Womens</p>
+		<img class="img" src="img/women/shoes/shoe3.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Nike Shoes Women</p>
+		<img class="img" src="img/women/shoes/shoe4.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Pumps Heel Women</p>
+		<img class="img" src="img/women/shoes/shoe5.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Ankle Boots Women</p>
+		<img class="img" src="img/women/shoes/shoe7.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+</section>
+
+<section class="sec">	
+	<h2 class="header">Accessories</h2>
+	<div class="card">
+		<p class="inf">Gold Bangles Set Womens</p>
+		<img class="img" src="img/women/accessories/1.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Vintage HeadBand Womens</p>
+		<img class="img" src="img/women/accessories/2.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">HairClips Set Womens</p>
+		<img class="img" src="img/women/accessories/3.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Alloy Earrings Womens</p>
+		<img class="img" src="img/women/accessories/4.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Smart Watch Womens</p>
+		<img class="img" src="img/women/accessories/7.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Crystal Earrings Womens</p>
+		<img class="img" src="img/women/accessories/6.jpeg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+</section>
+
+<section class="sec">	
+	<h2 class="header">Bags</h2>
+	<div class="card">
+		<p class="inf">ChloeDrew HandBag Womens</p>
+		<img class="img" src="img/women/bags/bag1.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Furla HandBag Womens</p>
+		<img class="img" src="img/women/bags/bag2.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Black Leather Bag Women</p>
+		<img class="img" src="img/women/bags/bag3.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Gold Shimmer Bags Womens</p>
+		<img class="img" src="img/women/bags/bag4.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Fancy Leather BagPack</p>
+		<img class="img" src="img/women/bags/bag5.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+	<div class="card">
+		<p class="inf">Prada Velvet Bags Womens</p>
+		<img class="img" src="img/women/bags/bag6.jpg">
+		<p class="inf"><img class="wish" src="img/wishlist.png" > 84.00 $<img class="cart" src="img/addtocart.png" ></p>
+	</div>
+</section>
+</body>
+</html>
