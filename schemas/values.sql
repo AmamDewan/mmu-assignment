bd11126bcb8e6002195cde30a30211c16e8bb6f9
Sunbeam20 <1191301017@student.mmu.edu.my>
2021-02-17 02:30:32 +0800

    images

---
+++ schemas/values.sql
@@ -0,0 +1,83 @@
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (101,"BWC Mens Watch Mens",value-3,value-4,"img/m/a/ma1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (102,"UV Protected Googles Mens",value-3,value-4,"img/m/a/ma2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (103,"Neck Tie Mens",value-3,value-4,"img/m/a/ma3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (104,"BaseBall Cap Mens",value-3,value-4,"img/m/a/ma4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (105,"Noblag Luxury Watch Men",value-3,value-4,"img/m/a/ma5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (106,"Italian Burnished Belt Men",value-3,value-4,"img/m/a/ma6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (107,"Vintage Leather Bagpack Mens",value-3,value-4,"img/m/b/mb1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (108,"Roxtons Bagpack Mens",value-3,value-4,"img/m/b/mb2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (109,"Leather Side Bagpack Men",value-3,value-4,"img/m/b/mb3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (110,"Vintage Luggage Mens",value-3,value-4,"img/m/b/mb4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (111,"Stylish Messenger Bags Mens",value-3,value-4,"img/m/b/mb5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (112,"Retro Sling Bags Mens",value-3,value-4,"img/m/b/mb6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (113,"Ann Klein Loafer Mens",value-3,value-4,"img/m/s/ms1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (114,"High Ankle Boots Mens",value-3,value-4,"img/m/s/ms2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (115,"Casual Loafer Mens",value-3,value-4,"img/m/s/ms3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (116,"Korean Outdoor Shoe Mens",value-3,value-4,"img/m/s/ms4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (117,"Nike Air Force 1 Mens",value-3,value-4,"img/m/s/ms5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (118,"Soft Leather Sneakers Mens",value-3,value-4,"img/m/s/ms6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (119,"Ann Klein Loafer Mens",value-3,value-4,"img/m/w/mw1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (120,"High Ankle Boots Mens",value-3,value-4,"img/m/w/mw2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (121,"Casual Loafer Mens",value-3,value-4,"img/m/w/mw3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (122,"Korean Outdoor Shoe Mens",value-3,value-4,"img/m/w/mw4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (123,"Nike Air Force 1 Mens",value-3,value-4,"img/m/w/mw5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (124,"Soft Leather Sneakers Mens",value-3,value-4,"img/m/w/mw6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (125,"Gold Bangles Set Womens",value-3,value-4,"img/w/a/wa1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (126,"Vintage HeadBand Womens",value-3,value-4,"img/w/a/wa2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (127,"HairClips Set Womens",value-3,value-4,"img/w/a/wa3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (128,"Alloy Earrings Womens",value-3,value-4,"img/w/a/wa4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (129,"Smart Watch Womens",value-3,value-4,"img/w/a/wa5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (130,"Crystal Earrings Womens",value-3,value-4,"img/w/a/wa6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (131,"ChloeDrew HandBag Womens",value-3,value-4,"img/w/b/wb1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (132,"Furla HandBag Womens",value-3,value-4,"img/w/b/wb2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (133,"Black Leather Bag Women",value-3,value-4,"img/w/b/wb3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (134,"Gold Shimmer Bags Womens",value-3,value-4,"img/w/b/wb4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (135,"Fancy Leather BagPack",value-3,value-4,"img/w/b/wb5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (136,"Prada Velvet Bags Womens",value-3,value-4,"img/w/b/wb6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (137,"Glittery Peep Heels Womens",value-3,value-4,"img/w/s/ws1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (138,"Sweet Womens High Heel",value-3,value-4,"img/w/s/ws2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (139,"Gucci Ankle Boot Womens",value-3,value-4,"img/w/s/ws3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (140,"Nike Shoes Women",value-3,value-4,"img/w/s/ws4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (141,"Pumps Heel Women",value-3,value-4,"img/w/s/ws5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (142,"Ankle Boots Women",value-3,value-4,"img/w/s/ws6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (143,"Korean Womens Blouse",value-3,value-4,"img/w/w/ww1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (144,"Formal Two Pieces Womens",value-3,value-4,"img/w/w/ww2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (145,"Stretchy Gym Tights Womens",value-3,value-4,"img/w/w/ww3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (146,"Peplum Suits Womens",value-3,value-4,"img/w/w/ww4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (147,"Korean Velvet Dress Womens",value-3,value-4,"img/w/w/ww5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (148,"SweatShirt Womens",value-3,value-4,"img/w/w/ww6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (149,"Kids Hair Pins",value-3,value-4,"img/k/a/ka1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (150,"Hats Kids",value-3,value-4,"img/k/a/ka2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (151,"Navigator Sunglass Kids",value-3,value-4,"img/k/a/ka3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (152,"Sustainable Sunglass",value-3,value-4,"img/k/a/ka4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (153,"Table & Chairs Set Kids",value-3,value-4,"img/k/a/ka5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (154,"Adjustable Table & Chair Set",value-3,value-4,"img/k/a/ka6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (155,"SchoolBag Dinosaur Painting",value-3,value-4,"img/k/b/kb1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (156,"Kindergarten Bagpack",value-3,value-4,"img/k/b/kb2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (157,"Kids Mini Shoulder Bag",value-3,value-4,"img/k/b/kb3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (158,"Kindergarten SchoolBag",value-3,value-4,"img/k/b/kb4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (159,"Tatonka Husky Bag Kids",value-3,value-4,"img/k/b/kb5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (160,"Kids Cute Luggage Set",value-3,value-4,"img/k/b/kb6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (161,"Flat Heel Kids Sandals",value-3,value-4,"img/k/s/ks1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (162,"Kids Rubber Sneakers",value-3,value-4,"img/k/s/ks2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (163,"Kids Led Shoes",value-3,value-4,"img/k/s/ks3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (164,"Pikachu Converse For kids",value-3,value-4,"img/k/s/ks4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (165,"Casual Running Shoes",value-3,value-4,"img/k/s/ks5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (166,"Sandal Spiderman Printed",value-3,value-4,"img/k/s/ks6.jpg",84.00);
+
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (167,"Short Sleeve Loose T-Shirt",value-3,value-4,"img/k/w/kw1.jpg",333.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (168,"Cute NightDress Girls",value-3,value-4,"img/k/w/kw2.jpg",125.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (169,"Printed Sweatshirt Kids",value-3,value-4,"img/k/w/kw3.jpg",89.50);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (170,"Denim Outfit Kids",value-3,value-4,"img/k/w/kw4.jpg",53.99);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (171,"Emboidered Dress Girls",value-3,value-4,"img/k/w/kw5.jpg",279.00);
+INSERT INTO `product`(`id`, `title`, `slug`, `sku`, `imgUrl`, `price`) VALUES (172,"Girls Summer Check Clothes",value-3,value-4,"img/k/w/kw6.jpg",84.00);
